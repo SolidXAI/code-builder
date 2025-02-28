e19508d9c0ce5ee2e9d9b76d80365fb2846fea92
Oswald Rodrigues <oswald@logicloop.io>
2025-02-28 14:25:27 +0530

    flatten inverse field changes for many-to-one dto

--- src/code-builder/lib/field/field-managers/relation/ManyToOneRelationFieldManagerForDto.ts
+++ src/code-builder/lib/field/field-managers/relation/ManyToOneRelationFieldManagerForDto.ts
@@ -1,12 +1,9 @@
-import { classify, dasherize } from '@angular-devkit/core/src/utils/strings';
+import { classify } from '@angular-devkit/core/src/utils/strings';
 import { Tree } from '@angular-devkit/schematics';
 import ts, { PropertyDeclaration } from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
-import { Change } from '@schematics/angular/utility/change';
-import { ArrayDecoratorManager } from '../../decorator-managers/dto/ArrayDecoratorManager';
 import { OptionalDecoratorManager } from '../../decorator-managers/dto/OptionalDecoratorManager';
 import { StringDecoratorManager } from '../../decorator-managers/dto/StringDecoratorManager';
-import { TransformDecoratorManager } from '../../decorator-managers/dto/TransformDecoratorManager';
-import { DecoratorManager, DecoratorType, DtoSourceType, FieldChange, FieldManager, FieldType, ManagerForDtoOptions, createSourceFile, safeInsertImport } from '../../FieldManager';
+import { FieldChange, FieldManager, FieldType, ManagerForDtoOptions } from '../../FieldManager';
 import { BaseFieldManagerForDto } from '../base/BaseFieldManagerForDto';
 
 export class ManyToOneRelationFieldManagerForDto
@@ -18,31 +15,9 @@ export class ManyToOneRelationFieldManagerForDto
   isBoolean(): boolean {
     return false;
   }
-  relationInverseSource: ts.SourceFile;
-  relationInverseDecoratorManagers: DecoratorManager[];
 
   constructor(tree: Tree, moduleName: string, modelName: string, field: any, options: ManagerForDtoOptions) {
     super(tree, moduleName, modelName, {...field, required: false}, options);
-    if (this.field.relationCreateInverse) {
-      const relatedEntityFileName = (this.options.sourceType === DtoSourceType.Create) ? `create-${dasherize(this.field.relationModelSingularName)}.dto.ts` : `update-${dasherize(this.field.relationModelSingularName)}.dto.ts`;
-      const relatedEntityPath = this.field.relationModelModuleName ? `src/${dasherize(this.field.relationModelModuleName)}/dtos/${relatedEntityFileName}` : `src/${dasherize(moduleName)}/dtos/${relatedEntityFileName}`;
-      this.relationInverseSource = createSourceFile(tree, relatedEntityPath);
-      this.relationInverseDecoratorManagers = this.getFieldDecoratorManagers(
-        this.field,
-        this.relationInverseSource,
-        DecoratorType.Array,
-        DecoratorType.ValidateNested,
-        // DecoratorType.Transform,
-      );
-      //HACK: This is a hack to add the transform decorator to the inverse field
-      this.relationInverseDecoratorManagers.push(
-        new TransformDecoratorManager({ isTransform: true, type: this.transformType(), source: this.relationInverseSource, field: field })
-      );
-      //HACK: This is a hack to replace the optional decorator to the inverse field
-      this.relationInverseDecoratorManagers.push(
-        new OptionalDecoratorManager({ isApplyOptional: true, optional: true, source: this.relationInverseSource, field: field })
-      );
-    }
   }
 
   isString(): boolean {
@@ -87,35 +62,11 @@ export class ManyToOneRelationFieldManagerForDto
     };
   }
 
-  // This field type is used in the case of one-to-many relation, when we generate the inverse field in the related entity
-  additionalFieldType(): FieldType {
-    const type = this.transformType() //TODO Discuss if this needs to be Create Or Update DTO
-    const text = `${type}[]`
-    return {
-      text: text,
-      node: (_field: any) =>
-        ts.factory.createArrayTypeNode(ts.factory.createTypeReferenceNode(
-          ts.factory.createIdentifier(type),
-          undefined
-        )),
-    };
-  }
-
-  private transformType(): string {
-    return  `Update${classify(this.modelName)}Dto`;
-  }
-
   override addAdditionalField(): FieldChange[] {
     const fieldChanges: FieldChange[] = [];
 
     // Add the userKey field to the main entity
     fieldChanges.push(this.addAdditionalUserKeyField());
-
-    if (this.field.relationCreateInverse) {
-      fieldChanges.push(this.addAdditionalInverseField());
-      fieldChanges.push(this.addAdditionalInverseIdsField());
-      fieldChanges.push(this.addAdditionalInverseCommandsField());
-    }
 
     return fieldChanges;
   }
@@ -136,71 +87,12 @@ export class ManyToOneRelationFieldManagerForDto
     return fieldChange;
   }
   
-  private addAdditionalInverseCommandsField(): FieldChange {
-    const fieldName = `${this.field.relationModelFieldName ?? this.modelName}Command`;
-    const fieldType = "string";
-    const source = this.relationInverseSource;
-    const field = this.field;
-    const modelName = this.field.relationModelSingularName;
-    const decoratorManagers = [
-      new StringDecoratorManager({ isString: true, source: source, field: field }),
-      new OptionalDecoratorManager({ isApplyOptional: true, optional: true, source: source, field: field }),
-      // TODO pending @IsEnum(RelationFieldsCommand) 
-    ];
-
-    //console.log(`\ncreate Dto addAdditionalInverseCommandsField ${fieldName} called ...`);
-
-    const fieldChange = this.addFieldInternal(fieldName, fieldType, decoratorManagers, field, modelName, source);
-
-    return fieldChange;
-  }
-
-  private addAdditionalInverseIdsField(): FieldChange {
-    const fieldName = `${this.field.relationModelFieldName ?? this.modelName}Ids`;
-    const fieldType = "number[]";
-    const source = this.relationInverseSource;
-    const field = this.field;
-    const modelName = this.field.relationModelSingularName;
-    const decoratorManagers = [
-      new OptionalDecoratorManager({ isApplyOptional: true, optional: true, source: source, field: field }),
-      new ArrayDecoratorManager({ isArray: true, source: source, field: field })
-    ]
-
-    //console.log(`\ncreate Dto addAdditionalInverseIdsField ${fieldName} called ...`);
-
-    const fieldChange = this.addFieldInternal(fieldName, fieldType, decoratorManagers, field, modelName, source);
-
-    return fieldChange;
-  }
-
-  private addAdditionalInverseField() {
-    const fieldName = this.additionalFieldName();
-    const fieldType = this.additionalFieldType().text;
-    const source = this.relationInverseSource;
-    const field = this.field;
-    const modelName = this.field.relationModelSingularName;
-    const decoratorManagers = this.relationInverseDecoratorManagers;
-    //console.log(`\ncreate Dto addAdditionalInverseField ${fieldName} called ...`);
-    const fieldChange = this.addFieldInternal(fieldName, fieldType, decoratorManagers, field, modelName, source);
-
-    if (this.modelName !== this.field.relationModelSingularName) {
-      const currentModelName = this.modelName;
-      const currentModuleName = this.moduleName;
-      fieldChange.changes.push(this.inverseFieldImport(currentModelName, currentModuleName, source));
-    }
-    return fieldChange;
-  }
 
   override addOrUpdateAdditionalField(): FieldChange[] {
     const fieldChanges: FieldChange[] = [];
 
     // Add the userKey field to the main entity
     fieldChanges.push(this.updateAdditionalUserKeyField());
-    if (this.field.relationCreateInverse) {
-      fieldChanges.push(this.updateAdditionalInverseField());
-      fieldChanges.push(this.updateAdditionalInverseIdsField());
-      fieldChanges.push(this.updateAdditionalInverseCommandsField());
-    }
     return fieldChanges;
   }
 
@@ -230,126 +122,26 @@ export class ManyToOneRelationFieldManagerForDto
         }
   }
 
-  private updateAdditionalInverseCommandsField(): FieldChange {
-
-        const commandFieldName = `${this.field.relationModelFieldName ?? this.modelName}Command`
-        const commandFieldType = ts.factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword);
-        const source = this.relationInverseSource
-        const field = this.field
-        const decoratorManagers = [
-            new StringDecoratorManager({ isString: true, source: source, field: field }),
-            new OptionalDecoratorManager({ isApplyOptional: true, optional: true, source: source, field: field }),
-            // TODO pending @IsEnum(RelationFieldsCommand) 
-        ]
- 
-        const commandField = this.getFieldIdentifierNode(
-            commandFieldName,
-            source
-        )?.parent as PropertyDeclaration;
-        //console.log(`\ncreate Dto updateAdditionalInverseCommandsField ${commandFieldName} called ...`);
-
-        if (commandField == null) {
-            return this.addAdditionalInverseCommandsField();
-        }
-        else {
-            //Update the command field
-            return this.updateFieldInternal(commandFieldName, commandFieldType, decoratorManagers, field, source)
-        }
-  }
-
-  private updateAdditionalInverseIdsField(): FieldChange {
-    const idsFieldName = `${this.field.relationModelFieldName ?? this.modelName}Ids`
-    const idsFieldType = ts.factory.createArrayTypeNode(ts.factory.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword));
-    const source = this.relationInverseSource
-    const field = this.field
-    const decoratorManagers = [
-        new OptionalDecoratorManager({ isApplyOptional: true, optional: true, source: source, field: field }),
-        new ArrayDecoratorManager({ isArray: true, source: source, field: field })
-    ]
-
-    const idsField = this.getFieldIdentifierNode(
-        idsFieldName,
-        source
-    )?.parent as PropertyDeclaration;
-    //console.log(`\ncreate Dto updateAdditionalInverseIdsField ${idsFieldName} called ...`);
-
-    if (idsField == null) {
-        return this.addAdditionalInverseIdsField();
-    }
-    else {
-        //Update the ids field
-        return this.updateFieldInternal(idsFieldName, idsFieldType, decoratorManagers, field, source)
-    }
-
-  }
-
-  private updateAdditionalInverseField() : FieldChange{
-    const fieldName = this.additionalFieldName();
-    const fieldType = this.additionalFieldType().node(this.field);
-    const source = this.relationInverseSource;
-    const field = this.field;
-    const decoratorManagers = this.relationInverseDecoratorManagers;
-   
-    const inverseField = this.getFieldIdentifierNode(fieldName, source)?.parent as PropertyDeclaration;
-    if (inverseField == null) {
-      return this.addAdditionalInverseField();
-    }
-    else {
-      const fieldChange = this.updateFieldInternal(fieldName, fieldType, decoratorManagers, field, source); 
-      if (this.modelName !== this.field.relationModelSingularName) {
-        const currentModelName = this.modelName;
-        const currentModuleName = this.moduleName;
-        fieldChange.changes.push(this.inverseFieldImport(currentModelName, currentModuleName, source));
-      }
-      return fieldChange;
-    }
-  }
-
-  private inverseFieldImport(modelName: string, currentModuleName: string,  source: ts.SourceFile): Change {
-    const inverseEntityImportName = `update-${dasherize(modelName)}.dto`;
-    const modulePath = `src/${currentModuleName}`;
-    
-    const inverseEntityPath = `${modulePath}/dtos/${inverseEntityImportName}`;
-
-    return safeInsertImport(source, `Update${classify(modelName)}Dto`, inverseEntityPath, currentModuleName);
-  }
 
   override removeAdditionalField(): FieldChange[] {
     const fieldChanges: FieldChange[] = [];
+    
+    fieldChanges.push(this.removeAdditionalUserKeyField());
 
-    if (this.field.relationCreateInverse) {
-      fieldChanges.push(this.removeAdditionalInverseField());
-      fieldChanges.push(this.removeAdditionalInverseIdsField());
-      fieldChanges.push(this.removeAdditionalInverseCommandsField());
-    }
     return fieldChanges;
   }
 
-  removeAdditionalInverseField(): FieldChange {
-    const fieldName = this.additionalFieldName();
-    const source = this.relationInverseSource;
+  private removeAdditionalUserKeyField(): FieldChange {
+    const fieldName = `${this.field.name}UserKey`;
+    const source = this.source;
     return this.removeFieldFor(fieldName, source);
   }
 
-  removeAdditionalInverseIdsField(): FieldChange {
-    const fieldName = `${this.field.relationModelFieldName ?? this.modelName}Ids`;
-    const source = this.relationInverseSource;
-    return this.removeFieldFor(fieldName, source);
-  }
-
-  removeAdditionalInverseCommandsField(): FieldChange {
-    const fieldName = `${this.field.relationModelFieldName ?? this.modelName}Command`;
-    const source = this.relationInverseSource;
-    return this.removeFieldFor(fieldName, source);
-  }
 
   override fieldName(): string {
     return `${this.field.name}Id`;
   }
 
-  additionalFieldName(): string {
-    return this.field.relationModelFieldName ?? `${this.modelName}s`;
-  }
 
   protected isAdditionalFieldRequired(): boolean {
     return true;
---
+++ src/code-builder/lib/field/field-managers/relation/OneToManyRelationFieldManagerForDto.ts
@@ -0,0 +1,252 @@
+import { classify, dasherize } from '@angular-devkit/core/src/utils/strings';
+import { Tree } from '@angular-devkit/schematics';
+import ts, { PropertyDeclaration } from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
+import { Change } from '@schematics/angular/utility/change';
+import { ArrayDecoratorManager } from '../../decorator-managers/dto/ArrayDecoratorManager';
+import { OptionalDecoratorManager } from '../../decorator-managers/dto/OptionalDecoratorManager';
+import { StringDecoratorManager } from '../../decorator-managers/dto/StringDecoratorManager';
+import { TransformDecoratorManager } from '../../decorator-managers/dto/TransformDecoratorManager';
+import { DecoratorType, FieldChange, FieldManager, FieldType, ManagerForDtoOptions, safeInsertImport } from '../../FieldManager';
+import { BaseFieldManagerForDto } from '../base/BaseFieldManagerForDto';
+
+export class OneToManyRelationFieldManagerForDto
+  extends BaseFieldManagerForDto
+  implements FieldManager {
+  isJson(): boolean {
+    return false;
+  }
+  isBoolean(): boolean {
+    return false;
+  }
+
+  constructor(tree: Tree, moduleName: string, modelName: string, field: any, options: ManagerForDtoOptions) {
+    super(tree, moduleName, modelName, { ...field, required: false }, options);
+    this.decoratorManagers = [...this.decoratorManagers, ...this.getFieldDecoratorManagers(
+      this.field,
+      this.source,
+      DecoratorType.Array,
+      DecoratorType.ValidateNested,
+    )];
+    this.decoratorManagers.push(
+      new TransformDecoratorManager({ isTransform: true, type: this.transformType(), source: this.source, field: field })
+    );
+  }
+
+  isString(): boolean {
+    return false;
+  }
+  isNumber(): boolean {
+    return false;
+  }
+  isInt(): boolean {
+    return true;
+  }
+  isDecimal(): boolean {
+    return false;
+  }
+  isApplyRegex(): boolean {
+    return false;
+  }
+  isApplyRequired(): boolean {
+    return true;
+  }
+  isApplyMin(): boolean {
+    return false;
+  }
+  isApplyMax(): boolean {
+    return false;
+  }
+  isDate(): boolean {
+    return false;
+  }
+  isTransform(): boolean {
+    return false;
+  }
+
+  // This field type is used in the case of one-to-many relation, when we generate the inverse field in the related entity
+  fieldType(): FieldType {
+    const type = this.transformType() //TODO Discuss if this needs to be Create Or Update DTO
+    const text = `${type}[]`
+    return {
+      text: text,
+      node: (_field: any) =>
+        ts.factory.createArrayTypeNode(ts.factory.createTypeReferenceNode(
+          ts.factory.createIdentifier(type),
+          undefined
+        )),
+    };
+  }
+
+  private transformType(): string {
+    return `Update${classify(this.field.relationModelSingularName)}Dto`;
+  }
+
+  override addField(): FieldChange[] {
+    const fieldChanges: FieldChange[] = super.addField();
+    if (fieldChanges.length > 0 && this.modelName !== this.field.relationModelSingularName) {
+      const mainField = fieldChanges[0];
+      mainField.changes.push(this.relatedFieldImport());
+    }
+    return fieldChanges;
+  }
+
+  override addAdditionalField(): FieldChange[] {
+    const fieldChanges: FieldChange[] = [];
+
+    // Add the ids field to the main entity
+    fieldChanges.push(this.addAdditionalIdsField());
+    // Add the command field to the main entity
+    fieldChanges.push(this.addAdditionalCommandField());
+
+    return fieldChanges;
+  }
+
+  private addAdditionalIdsField(): FieldChange {
+    const fieldName = `${this.field.name}Ids`;
+    const fieldType = "number[]";
+    const source = this.source
+    const field = this.field
+    const modelName = this.modelName
+    const decoratorManagers = [
+      new OptionalDecoratorManager({ isApplyOptional: true, optional: true, source: source, field: field }),
+      new ArrayDecoratorManager({ isArray: true, source: source, field: field })
+    ]
+
+    return this.addFieldInternal(fieldName, fieldType, decoratorManagers, field, modelName, source);
+  }
+
+  private addAdditionalCommandField(): FieldChange {
+    const fieldName = `${this.field.name}Command`;
+    const fieldType = "string";
+    const source = this.source
+    const field = this.field
+    const modelName = this.modelName
+    const decoratorManagers = [
+      new StringDecoratorManager({ isString: true, source: source, field: field }),
+      new OptionalDecoratorManager({ isApplyOptional: true, optional: true, source: source, field: field }),
+      // TODO pending @IsEnum(RelationFieldsCommand) 
+    ]
+
+    return this.addFieldInternal(fieldName, fieldType, decoratorManagers, field, modelName, source);
+
+  }
+
+
+  override updateField(): FieldChange[] {
+    const fieldChanges: FieldChange[] = super.updateField();
+
+    //FIXME This might not be required, since addField might never be called from within updateField
+    // const containsAddFieldChanges = fieldChanges.filter((change) => !(change instanceof InsertChange));
+    // if (containsAddFieldChanges) return fieldChanges;
+
+    //This line is required to add import changes in the update context
+    // if (fieldChanges.length > 0 && this.modelName !== this.field.relationModelSingularName) {
+    const mainField = fieldChanges[0];
+    mainField.changes.push(this.relatedFieldImport());
+    // }
+    return fieldChanges
+  }
+
+  override addOrUpdateAdditionalField(): FieldChange[] {
+    const fieldChanges: FieldChange[] = [];
+
+    // Add or update the ids field
+    fieldChanges.push(this.updateAdditionalIdsField());
+    // Add or update the command field
+    fieldChanges.push(this.updateAdditionalCommandField());
+
+    return fieldChanges;
+  }
+
+  private updateAdditionalIdsField(): FieldChange {
+    const idsFieldName = `${this.field.name}Ids`
+    const source = this.source
+    const field = this.field
+
+    // Handle the main source additional fields
+    // Add or update the ids field
+    const idsField = this.getFieldIdentifierNode(
+      idsFieldName,
+      source
+    )?.parent as PropertyDeclaration;
+    if (idsField == null) {
+      return this.addAdditionalIdsField();
+    }
+    else {
+      //Update the ids field
+      const fieldType = ts.factory.createArrayTypeNode(ts.factory.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword));
+      const decoratorManagers = [
+        new OptionalDecoratorManager({ isApplyOptional: true, optional: true, source: source, field: field }),
+        new ArrayDecoratorManager({ isArray: true, source: source, field: field })
+      ];
+      return this.updateFieldInternal(idsFieldName, fieldType, decoratorManagers, field, source);
+    }
+
+  }
+
+  updateAdditionalCommandField(): FieldChange {
+    const commandFieldName = `${this.field.name}Command`
+    const source = this.source
+    const field = this.field
+
+    // Handle the main source additional fields
+    // Add or update the command field
+    const commandField = this.getFieldIdentifierNode(
+      commandFieldName,
+      source
+    )?.parent as PropertyDeclaration;
+    if (commandField == null) {
+      return this.addAdditionalCommandField();
+    }
+    else {
+      //Update the command field
+      const fieldType = ts.factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword);
+      const decoratorManagers = [
+        new StringDecoratorManager({ isString: true, source: source, field: field }),
+        new OptionalDecoratorManager({ isApplyOptional: true, optional: true, source: source, field: field }),
+        // TODO pending @IsEnum(RelationFieldsCommand) 
+      ];
+      return this.updateFieldInternal(commandFieldName, fieldType, decoratorManagers, field, source);
+    }
+  }
+
+
+  override removeAdditionalField(): FieldChange[] {
+    const fieldChanges: FieldChange[] = [];
+
+    // Add the ids field to the main entity
+    fieldChanges.push(this.removeAdditionalIdsField());
+    // Add the command field to the main entity
+    fieldChanges.push(this.removeAdditionalCommandField());
+
+    return fieldChanges;
+  }
+
+  removeAdditionalIdsField(): FieldChange {
+    const fieldName = `${this.field.name}Ids`;
+    const source = this.source;
+    return this.removeFieldFor(fieldName, source);
+  }
+  removeAdditionalCommandField(): FieldChange {
+    const fieldName = `${this.field.name}Command`;
+    const source = this.source;
+    return this.removeFieldFor(fieldName, source);
+  }
+
+
+  override fieldName(): string {
+    return `${this.field.name}Id`;
+  }
+
+  protected isAdditionalFieldRequired(): boolean {
+    return true;
+  }
+
+  relatedFieldImport(): Change {
+    const relatedEntityImportName = `update-${dasherize(this.field.relationModelSingularName)}.dto`;
+    const relatedEntityPath = this.field.relationModelModuleName ? `src/${this.field.relationModelModuleName}/dtos/${relatedEntityImportName}` : `./${relatedEntityImportName}`;
+    return safeInsertImport(this.source, `Update${classify(this.field.relationModelSingularName)}Dto`, relatedEntityPath, this.moduleName);
+    // return insertImport(this.source, this.source.fileName, `Update${classify(this.field.relationModelSingularName)}Dto`, relatedEntityPath);
+  } //Uncomment this method while implementing many-to-many relation changes
+
+}
