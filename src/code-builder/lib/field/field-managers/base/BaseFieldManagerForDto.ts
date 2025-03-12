d52f97689e480f2fc3f66e76cc49d19814d08b09
Rajesh Chityal <rajesh.chityal@logicloop.io>
2025-03-12 17:35:22 +0530

    Default value changes retrevie

--- src/code-builder/lib/field/field-managers/base/BaseFieldManagerForDto.ts
+++ src/code-builder/lib/field/field-managers/base/BaseFieldManagerForDto.ts
@@ -188,7 +188,7 @@ export abstract class BaseFieldManagerForDto implements FieldManager {
 
   private buildPropertyLine(fieldName: string, fieldType: string, defaultConfigValue: string) {
     let entityPropertyLine = `${fieldName}: ${fieldType}`;
-    if (false) {
+    if (this.options.sourceType === DtoSourceType.Create) {
       const defaultValue = this.defaultValueInitializer(defaultConfigValue)?.text ?? null;
       if (defaultValue) {
         entityPropertyLine += ` = ${defaultValue}`;
