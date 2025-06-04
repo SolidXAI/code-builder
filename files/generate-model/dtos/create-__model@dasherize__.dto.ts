89a8601baccdf67cde797a5f60eee127061046fb
Oswald Rodrigues <oswald@logicloop.io>
2025-06-04 16:47:37 +0530

    codegen changes for the draft publish workflow

--- files/generate-model/dtos/create-__model@dasherize__.dto.ts
+++ files/generate-model/dtos/create-__model@dasherize__.dto.ts
@@ -1,2 +1,16 @@
 <%= outputParentImportPathForDto(parentModel ,parentModule ,"create") %>
-export class Create<%= classify(model) %>Dto<%= parentModel ? ` extends Create${classify(parentModel)}Dto` : `` %> {}
+<% if (draftPublishWorkflowEnabled) { %>
+import { IsOptional, IsDate } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { datetimeTransformer } from '@solidstarters/solid-core';
+<% } %>
+export class Create<%= classify(model) %>Dto<%= parentModel ? ` extends Create${classify(parentModel)}Dto` : `` %> {
+<% if (draftPublishWorkflowEnabled) { %>
+    @IsOptional()
+    @IsDate()
+    @ApiProperty()
+    @Transform(datetimeTransformer)
+    publishedAt: Date;
+<% } %>    
+}
