efeba04b4f63e9c35213a4f2072f61f4a4a976b7
Oswald Rodrigues <oswald@logicloop.io>
2025-06-11 10:04:12 +0530

    fixes around draft publish code generation

--- files/generate-model/dtos/create-__model@dasherize__.dto.ts
+++ files/generate-model/dtos/create-__model@dasherize__.dto.ts
@@ -1,12 +1,12 @@
 <%= outputParentImportPathForDto(parentModel ,parentModule ,"create") %>
-<% if (draftPublishWorkflowEnabled) { %>
+<% if (draftPublishWorkflowEnabled === 'true') { %>
 import { IsOptional, IsDate } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { datetimeTransformer } from '@solidstarters/solid-core';
 <% } %>
 export class Create<%= classify(model) %>Dto<%= parentModel ? ` extends Create${classify(parentModel)}Dto` : `` %> {
-<% if (draftPublishWorkflowEnabled) { %>
+<% if (draftPublishWorkflowEnabled === 'true') { %>
     @IsOptional()
     @IsDate()
     @ApiProperty()
--- files/generate-model/dtos/update-__model@dasherize__.dto.ts
+++ files/generate-model/dtos/update-__model@dasherize__.dto.ts
@@ -1,6 +1,6 @@
 import { IsInt,IsOptional } from 'class-validator';
-<% if (draftPublishWorkflowEnabled) { %>
-import { IsOptional, IsDate } from 'class-validator';
+<% if (draftPublishWorkflowEnabled === 'true') { %>
+import { IsDate } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { datetimeTransformer } from '@solidstarters/solid-core';
@@ -10,7 +10,7 @@ export class Update<%= classify(model) %>Dto<%= parentModel ? ` extends Update${
     @IsOptional()
     @IsInt()
     id: number;
-<% if (draftPublishWorkflowEnabled) { %>
+<% if (draftPublishWorkflowEnabled === 'true') { %>
     @IsOptional()
     @IsDate()
     @ApiProperty()
